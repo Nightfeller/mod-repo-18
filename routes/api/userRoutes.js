a05390c7fffacb84fbdb84835585c169c4c49da5
Nightfeller <family2earls@gmail.com>
2024-01-03 15:30:13 -0600

    added friend routes (haven't tested yet)

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -36,15 +36,19 @@ router.route('/:userId').get(async (req, res) => { try {
         const user = await User.findOneAndDelete({ _id: req.params.userId });
         if (!user) { res.status(404).json({ message: 'No user with that ID' }); }
         await Student.deleteMany({ _id: { $in: user.thoughts } });
-        res.json({ message: 'User and thoughts deleted!' });
+        res.json({ message: 'User and thoughts deleted.' });
     } catch (err) { res.status(500).json(err); }
 });
 
 router.route('/:userId/friends/:friendId').post(async (req, res) => { try {
-        const friend = await User.create(req.body);
+    const friend = await User.findOne({ _id: req.params.userId }).select('-__v');
+    if (!friend) { return res.status(404).json({ message: 'No friend with that ID' }); }
+    res.json(friend);
     } catch (err) { res.status(500).json(err); }
 }).delete(async (req, res) => { try {
-
+    const friend = await User.findOneAndDelete({ _id: req.params.userId });
+    if (!friend) { res.status(404).json({ message: 'No friend with that ID' }); }
+    res.json({ message: 'Friend remvoed.' });
     } catch (err) { res.status(500).json(err); }
 });
 
